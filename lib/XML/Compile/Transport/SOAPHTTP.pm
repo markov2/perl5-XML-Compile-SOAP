b8541b39604a0bb0718ad5d28bba9718f405fe9f
Mark Overmeer <mark@overmeer.net>
2019-11-20 08:43:41 +0100

    mime types

--- lib/XML/Compile/Transport/SOAPHTTP.pm
+++ lib/XML/Compile/Transport/SOAPHTTP.pm
@@ -221,11 +221,11 @@ sub _prepare_call($)
 
     my $content_type;
     if($version eq 'SOAP11')
-    {   $mime  ||= ref $soap ? $soap->mimeType : 'text/xml';
+    {   $mime  ||= 'text/xml';
         $content_type = qq{$mime; charset=$charset};
     }
     elsif($version eq 'SOAP12')
-    {   $mime  ||= ref $soap ? $soap->mimeType : 'application/soap+xml';
+    {   $mime  ||= 'application/soap+xml';
         my $sa   = defined $action ? qq{; action="$action"} : '';
         $content_type = qq{$mime; charset=$charset$sa};
         $header->header(Accept => $mime);  # not the HTML answer
